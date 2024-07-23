1984da193d0a940dc8932f70cba8b5cc6be3d7f9
Mussie <musdzion@gmail.com>
2024-07-23 05:01:21 -0400

    add logic to methods in filter

--- filters.rb
+++ filters.rb
@@ -3,14 +3,32 @@
 
 def find(id)
     # Your code Here
+   @candidates.find {|candidate| candidate[:id] == id}
+    
   end
   
   def experienced?(candidate)
     # Your code Here
+    candidate[:years_of_experience] >= 2
+    
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+      candidate[:github_points] >= 100 &&
+      (candidate[:languages].include?("Ruby")||
+      candidate[:languages].include?("Python")) &&
+      candidate[:date_applied] >= 15.days.ago.to_date && 
+      candidate[:age] >= 18
+    end
+    
   end
   
   # More methods will go below
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate|
+  [-candidate[:years_of_experience], -candidate[:github_points]]}
+  end
